08df70d13b2763eeba016c99442fedd15b174c49
Ben Martinez-Bateman <ben@alwaysblank.org>
2020-11-25 19:08:28 -0800

    Initial form, etc

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,10 +6,25 @@ module.exports = {
     ],
     darkMode: 'media',
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                gold: '#f6ae2d',
+                lavender: 'rgba(174, 138, 174, 0.35)',
+            },
+            fontFamily: {
+                heading: "'Texturina', serif",
+                body: "'Nunito Sans', sans-serif",
+            },
+            fontSize: {
+                homeHeading: '6rem',
+            }
+        },
     },
     variants: {
         extend: {},
     },
-    plugins: [],
+    plugins: [
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/typography'),
+    ],
 }
